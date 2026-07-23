ab3ea3ddd390360c1772aac0177daebb56202ec0
Hawa <laprincesseawa99@gmail.com>
2026-07-23 11:00:26 +0000

    application bonjour

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+console.log("Bonjour les amis du village , awa va venir bientot");
+app.listen(3000, () =>{
+console.log("application demarrée sur le port 3000");
+});
+
